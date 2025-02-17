2ba4ec1e3f2352f03993e30b965ec6c5bf01e874
briancosentino <cosentinobrian@gmail.com>
2025-02-17 15:43:43 +0100

    add exercise 4

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function countVocals (word){
+    const vocals = ['a', 'e', 'i', 'o', 'u'];
+    let sumVocals = 0;
+    for (let i = 0; i < word.length; i++){
+        const thisLetter = word[i];
+        if (vocals.includes(thisLetter)){
+            sumVocals += 1
+        }
+    } return sumVocals
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(countVocals('madonnina'));
+ 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
