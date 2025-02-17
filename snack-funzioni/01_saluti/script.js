01bbff7f52677f333ab4ec7c758e4257b5b5e707
briancosentino <cosentinobrian@gmail.com>
2025-02-17 15:09:45 +0100

    first commit

---
+++ snack-funzioni/01_saluti/script.js
@@ -0,0 +1,16 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+
+
+// Dichiara la funzione qui.
+const userName = 'Mario';
+ function greetings (name){
+    console.log(`Ciao ${name}`);   
+ }
+ 
+
+// Invoca la funzione qui e stampa il risultato in console
+
+greetings(userName)
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
